31d7550c24365712a5121cc664c3910c2cce6333
David Thomson <31463870+MegaBoy117@users.noreply.github.com>
2024-06-18 23:07:12 +0100

    added basic adapters

--- shared/src/commonMain/kotlin/com/data/Adapters.kt
+++ shared/src/commonMain/kotlin/com/data/Adapters.kt
@@ -2,35 +2,36 @@ package com.data
 
 import app.cash.sqldelight.ColumnAdapter
 import com.benasher44.uuid.Uuid
+import com.benasher44.uuid.uuidFrom
 import com.chrynan.colors.Color
 import kotlinx.datetime.LocalDate
 
 val localDateAdapter = object : ColumnAdapter<LocalDate, String> {
     override fun decode(databaseValue: String): LocalDate {
-        TODO("Not yet implemented")
+        return LocalDate.parse(databaseValue)
     }
 
     override fun encode(value: LocalDate): String {
-        TODO("Not yet implemented")
+        return value.toString()
     }
 }
 
 val uuidAdapter = object : ColumnAdapter<Uuid, String> {
     override fun decode(databaseValue: String): Uuid {
-        TODO("Not yet implemented")
+        return uuidFrom(databaseValue)
     }
 
     override fun encode(value: Uuid): String {
-        TODO("Not yet implemented")
+        return value.toString()
     }
 }
 
 val colorAdapter = object : ColumnAdapter<Color, String> {
     override fun decode(databaseValue: String): Color {
-        TODO("Not yet implemented")
+        return Color(databaseValue)
     }
 
     override fun encode(value: Color): String {
-        TODO("Not yet implemented")
+        return Color.toString()
     }
 }
