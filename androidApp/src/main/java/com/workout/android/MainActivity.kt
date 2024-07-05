33577e344c86e7f9dcead57f8c3347abe05142f3
David Thomson <31463870+MegaBoy117@users.noreply.github.com>
2024-07-05 17:29:08 +0100

    added basic koin classes and initialisation to main activity

--- androidApp/src/main/java/com/workout/android/MainActivity.kt
+++ androidApp/src/main/java/com/workout/android/MainActivity.kt
@@ -2,39 +2,15 @@ package com.workout.android
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.workout.Greeting
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    GreetingView(Greeting().greet())
-                }
-            }
+        startKoin {
+            androidContext(this@MainActivity)
+            modules()
         }
-    }
-}
-
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
     }
 }
